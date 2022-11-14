ebb201217d9c703c0929519ed510c8d6a21c44cc
Adam Lievrouw <adamlievrouw@gmail.com>
2022-11-14 10:08:08 -0600

    a few changes

--- data-analysis/panda-testing.py
+++ data-analysis/panda-testing.py
@@ -3,17 +3,29 @@ import pandas as pd
 # Data set source
 # https://www.kaggle.com/datasets/rakkesharv/imdb-5000-movies-multiple-genres-dataset
 
-df = pd.read_csv('/Users/adam/Downloads/IMDb_All_Genres_etf_clean1.csv', header=0)
-new_headers = ['Movie_Title', 'Year', 'Director']
-dataset = pd.DataFrame(df, columns=new_headers)
+IMDb = '/Users/adam/Downloads/IMDb_All_Genres_etf_clean1.csv'
+df = pd.read_csv(IMDb)
+
+print("")
+print(df.columns)
+
+my_headers = ['Movie_Title', 'Year', 'Director', 'main_genre']
+ds = pd.DataFrame(df, columns=my_headers)
 print("")
 
 # Show the top 10 most common director's names
-common_directors = pd.Series(' '.join(dataset['Director']).split()).value_counts()[:10]
+common_directors = ds['Director'].value_counts().nlargest(10)
+# common_directors = pd.Series(' '.join(ds['Director']).split()).value_counts()[:10]
 print(common_directors)
 print("")
 
 # Print the top 10 most common movie release years
-common_years =  pd.Series(' '.join(dataset['Year']).split()).value_counts()[10:]
+common_years = ds['Year'].value_counts().nlargest(10)
 print(common_years)
 print("")
+
+# grouped_df = df.groupby(['Year']).max()
+# print(grouped_df['Movie_Title'])
+# print("")
+
+# ds.groupby('Movie_Title', 'Year', 'main_genre').
